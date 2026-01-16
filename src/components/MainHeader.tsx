65ad7d910729e9e67aa5d978efee853df9704a8d
84kb <84kilobytes@gmail.com>
2026-01-17 00:34:59 +0900

    更新ボタンの配置を変更

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -262,6 +262,19 @@ export function MainHeader({
                 </div>
 
                 <div className="header-right">
+                    {/* 更新ボタン */}
+                    <button
+                        className="icon-btn"
+                        title="更新"
+                        onClick={onRefresh}
+                    >
+                        <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                            <path d="M23 4v6h-6"></path>
+                            <path d="M1 20v-6h6"></path>
+                            <path d="M3.51 9a9 9 0 0 1 14.85-3.36L23 10M1 14l4.64 4.36A9 9 0 0 0 20.49 15"></path>
+                        </svg>
+                    </button>
+
                     {/* 表示オプションドロップダウン */}
                     <div className="view-menu-container" ref={viewMenuRef}>
                         <button
@@ -445,11 +458,6 @@ export function MainHeader({
                                     </label>
                                 </div>
 
-                                <div className="dropdown-divider"></div>
-
-                                <button className="dropdown-refresh-btn" onClick={onRefresh}>
-                                    更新
-                                </button>
                             </div>
                         )}
                     </div>
