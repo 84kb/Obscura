d50ea30cdc6c19494c7dcc6d0c620e400be57923
84kb <84kilobytes@gmail.com>
2026-01-20 05:39:14 +0900

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,4 +1,6 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")) { Start-Process powershell.exe "-File `"$PSCommandPath`"" -Verb RunAs; exit }
 cd E:\Projects\Obscura
+npm run clean
+rm -rf dist dist-electron
 Remove-Item -Recurse -Force .\release
 npm run electron:build
